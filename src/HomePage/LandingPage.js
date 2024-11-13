5f85b1034c3993481a85e101441d4b41f7fce734
shabd <shabdbhatia@gmail.com>
2024-11-13 21:43:49 +0530

    Base Working Code

---
+++ src/HomePage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landing_Section1 from './components/Landing_Section1';
+
+const LandingPage = () => {
+    return (
+        <div className="App">
+            <Landing_Section1/>
+        </div>
+    );
+};
+
+export default LandingPage;
---
+++ src/HomePage/components/Landing_Section1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing_Section1 = () => {
+    return (
+        <div className={'container'} id={'landing_s1_div'}>
+            <h1>Genesis 2024!</h1>
+        </div>
+    )
+}
+
+export default Landing_Section1;
