61d415d5345c28be7eff7c8f43c560eac5678e84
shabd <shabdbhatia@gmail.com>
2024-11-18 22:21:41 +0530

    Added Section 1 & Background & Logo

--- src/Header/navbar.js
+++ src/Header/navbar.js
@@ -1,36 +1,30 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './navbar_style.css'; // Import your CSS file
+import Genesis_Logo from './../Assets/Logos/Genesis_Logo_Transperant.png';
 
-        // <div className={'navbar'} id={'navbar_div'}>
-        //     <div className={'navbar_container'} id={'left_div_navbar'}>
-        //         <h1 className={'navbar_heading'} id={'nav_heading'}>Genesis 2024</h1>
-        //     </div>
-        //     <div className={'navbar-container'} id={'right_div_navbar'}>
-        //         <a className={'navbar_link'} id={'navbar_link-1'} href={'#'}>Home</a>
-        //         <a className={'navbar_link'} id={'navbar_link-2'} href={'#'}>About</a>
-        //         <a className={'navbar_link'} id={'navbar_link-3'} href={'#'}>Schedule</a>
-        //     </div>
-        // </div>
-        // src/Navbar.js
+function NavigationBar() {
+    return (
+        <Navbar className="custom-navbar" expand="md">
+            <Container>
+                {/*<Navbar.Brand href="#" className="Main-Headings" id="Navbar-Heading">*/}
+                {/*    Genesis 2024*/}
+                {/*</Navbar.Brand>*/}
+                <Navbar.Brand>
+                    <img src={Genesis_Logo} alt={'genesis_logo'} className={'navbar_logo'} id={'genesis_logo'}/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" className={"navbar-links"}>Home</Nav.Link>
+                        <Nav.Link href="#about" className={"navbar-links"}>About</Nav.Link>
+                        <Nav.Link href="#services" className={"navbar-links"}>Schedule</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
 
-        import React from 'react';
-        import { Navbar, Nav, Container } from 'react-bootstrap';
-        import 'bootstrap/dist/css/bootstrap.min.css';
-
-        function NavigationBar() {
-            return (
-                <Navbar bg="light" expand="md">
-                    <Container>
-                        <Navbar.Brand href="#">Genesis 2024</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#about">About</Nav.Link>
-                                <Nav.Link href="#services">Schedule</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            );
-        }
-
-        export default NavigationBar;
+export default NavigationBar;
--- src/HomePage/components/Landing_Section1.js
+++ src/HomePage/components/Landing_Section1.js
@@ -4,9 +4,6 @@ import './../styling/landing_section1_style.css';
 const Landing_Section1 = () => {
     return (
         <div className={'container'} id={'landing_s1_div'}>
-            <div className={'logo_div'} id={'logo_div_s1'}>
-                <img src={require('./../../Assets/Logo_Placeholder.jpeg')} alt={'genesis_logo'} className={'logo'} id={'genesis_s1_logo'}/>
-            </div>
             <div className={'heading_div'} id={'heading_div_s1'}>
                 <h1 className={'genesis_heading_h1'}>Genesis 2024</h1>
                 <h2 className={'genesis_heading_h2'}>December 7th 2024</h2>
