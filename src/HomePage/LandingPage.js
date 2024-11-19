271703c0025b267b92a5ca5a588f822a478c7f46
shabd <shabdbhatia@gmail.com>
2024-11-19 11:00:15 +0530

    Added Section 2

--- src/HomePage/LandingPage.js
+++ src/HomePage/LandingPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Landing_Section1 from './components/Landing_Section1';
+import Landing_Section2 from './components/Landing_Section2';
+import Landing_Section3 from './components/Landing_Section3';
 
 const LandingPage = () => {
     return (
         <div className="App">
             <Landing_Section1/>
+            <Landing_Section2/>
+            <Landing_Section3/>
         </div>
     );
 };
--- src/HomePage/components/Landing_Section1.js
+++ src/HomePage/components/Landing_Section1.js
@@ -5,11 +5,14 @@ const Landing_Section1 = () => {
     return (
         <div className={'container'} id={'landing_s1_div'}>
             <div className={'heading_div'} id={'heading_div_s1'}>
-                <h1 className={'genesis_heading_h1'}>Genesis 2024</h1>
+                <h1 className={'genesis_heading_h1'}>Genesis</h1>
+                <h1 className={'genesis_heading_h1'}>2024</h1>
                 <h2 className={'genesis_heading_h2'}>December 7th 2024</h2>
 
                 <div className={'button_div'} id={'button_div_register'}>
-                    <button className={'button_register'} onClick={() => {}} id={'register_button_s1'}>Register Now!</button>
+                    <button className={'button_register'} onClick={() => {
+                    }} id={'register_button_s1'}>Register Now!
+                    </button>
                 </div>
             </div>
         </div>
